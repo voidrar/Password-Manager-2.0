5d327b200483d32db031f67ab3668e3648db11e6
Void.7z <onyx5.0@Outlook.com>
2025-08-15 22:06:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,11 @@
 #include <iostream>
-#include <limits>	
-#include <stdlib.h>
-#include <string>
-#include <fstream>
 #include "passwords.h"
+#include "menu.h"
 #include "ViewPasswords.h"
 using namespace std;
-string menus;
-void menu()
+int main()
 {
-	std::cout << R"(
- /$$$$$$$$                               /$$$$$$$                           
-|_____ $$                               | $$__  $$                          
-     /$$/   /$$$$$$   /$$$$$$   /$$$$$$ | $$  \ $$  /$$$$$$  /$$   /$$      
-    /$$/   /$$__  $$ /$$__  $$ /$$__  $$| $$  | $$ |____  $$| $$  | $$      
-   /$$/   | $$$$$$$$| $$  \__/| $$  \ $$| $$  | $$  /$$$$$$$| $$  | $$      
-  /$$/    | $$_____/| $$      | $$  | $$| $$  | $$ /$$__  $$| $$  | $$      
- /$$$$$$$$|  $$$$$$$| $$      |  $$$$$$/| $$$$$$$/|  $$$$$$$|  $$$$$$$      
-|________/ \_______/|__/       \______/ |_______/  \_______/ \____  $$      
-                                                             /$$  | $$      
-                                                            |  $$$$$$/      
-                                                             \______/       
-)" << "\n";
-
-
-
-
-	cout << "are We viewing or setting a password? \n";
 	
-	cin >> menus;
-	system("cls");
-	if (menus == "Viewing" || menus == "viewing")
-	{
-		
-		ViewPassword();
-	}
-	else if (menus == "Setting" || menus == "setting")
-	{
-		system("cls");
-		CreatePassword();
-	}
-	else
-		menu();
+	menu();
+
 }
