3a3819d9d9181c769d6c67d285087a9a13dc0e40
Void.7z <onyx5.0@Outlook.com>
2025-08-15 22:06:29 -0700

    Update passwords.h

--- passwords.h
+++ passwords.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <string>
 
+using namespace std;
 void ViewPassword();
 void CreatePassword();
 void AddEmail2Pass();
+void Discord();
+void VRC();
+void Uni();
+void Decor();
+extern string CreatePass;
+extern string DiscordPass;
+extern string VRCPass;
+extern string Universal;
+extern string UV;
+
